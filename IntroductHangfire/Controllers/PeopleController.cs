c0651178252eab9bf1988dcbe2a77b59fcb0a070
holvon <holvon@outlook.com>
2023-05-21 23:16:15 +0800

    使用 hangfire 的一个例子, 依赖注入IBackgroundJobClient, 把任务加入 hangfire 的队列

--- IntroductHangfire/Controllers/PeopleController.cs
+++ IntroductHangfire/Controllers/PeopleController.cs
@@ -1,5 +1,6 @@
 namespace IntroductHangfire.Controllers;
 
+using Hangfire;
 using IntroductHangfire.Data;
 using IntroductHangfire.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -9,15 +10,23 @@ using Microsoft.AspNetCore.Mvc;
 public class PeopleController : ControllerBase
 {
     private readonly ApplicationDbContext _context;
+    private readonly IBackgroundJobClient backgroundJobClient;
 
-    public PeopleController(ApplicationDbContext dbContext)
+    public PeopleController(
+        ApplicationDbContext dbContext,
+        IBackgroundJobClient backgroundJobClient
+    )
     {
         _context = dbContext;
+        this.backgroundJobClient = backgroundJobClient;
     }
 
     [HttpPost("create")]
     public async Task<IActionResult> PostAsync(string personName)
     {
+        // 这样加入的任务是给了hangfire执行(作为后台任务执行)的, 可能这样子不会阻塞?
+        backgroundJobClient.Enqueue(() => Console.WriteLine(personName));
+
         System.Console.WriteLine("Adding person: " + personName);
         var person = new Person();
         _context.Add(person);
