4eb15a2705edd4cec26a020db477ca5c252a7484
holvon <holvon@outlook.com>
2023-05-21 23:39:53 +0800

    一个 hangfire 后台调用任务的例子

--- IntroductHangfire/Controllers/PeopleController.cs
+++ IntroductHangfire/Controllers/PeopleController.cs
@@ -22,20 +22,22 @@ public class PeopleController : ControllerBase
     }
 
     [HttpPost("create")]
-    public async Task<IActionResult> PostAsync(string personName)
+    public IActionResult PostAsync(string personName)
     {
-        // 这样加入的任务是给了hangfire执行(作为后台任务执行)的, 可能这样子不会阻塞?
-        backgroundJobClient.Enqueue(() => Console.WriteLine(personName));
+        // 只有 public 的方法才能给后台调用, 加入后台调用的任务不会阻塞进程
+        backgroundJobClient.Enqueue(() => CreatePersonAsync(personName));
+        return Ok();
+    }
 
-        System.Console.WriteLine("Adding person: " + personName);
+    public async Task CreatePersonAsync(string personName)
+    {
+        Console.WriteLine("Adding person: " + personName);
         var person = new Person();
         _context.Add(person);
         // 假设的耗时任务
         await Task.Delay(5000);
         await _context.SaveChangesAsync();
         Console.WriteLine($"Add the person: {person}");
-
-        return Ok();
     }
 
     [HttpGet]
